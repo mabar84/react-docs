2eab7cd454ed39babedd6d845e2bc590cec4e22f
mabar84 <mabar@tut.by>
2024-02-11 21:43:36 +0300

    Challenge 1 of 4: Fix a broken chat input is done

--- src/items/RefereningValuesWithRefs.jsx
+++ src/items/RefereningValuesWithRefs.jsx
@@ -1,16 +1,17 @@
 import {useState} from "react";
-
+// Challenge 1 of 4: Fix a broken chat input
+// Type a message and click “Send”. You will notice there is a three second delay before you see the “Sent!” alert. During this delay, you can see an “Undo” button. Click it. This “Undo” button is supposed to stop the “Sent!” message from appearing. It does this by calling clearTimeout for the timeout ID saved during handleSend. However, even after “Undo” is clicked, the “Sent!” message still appears. Find why it doesn’t work, and fix it.
 export default function Chat() {
     const [text, setText] = useState('');
     const [isSending, setIsSending] = useState(false);
-    let timeoutID = null;
+    const [timeoutID, setTimeoutID] = useState(null);
 
     function handleSend() {
         setIsSending(true);
-        timeoutID = setTimeout(() => {
+        setTimeoutID(setTimeout(() => {
             alert('Sent!');
             setIsSending(false);
-        }, 3000);
+        }, 1500))
     }
 
     function handleUndo() {
